28f76f93ae88ece8005e3cc71fa4ec8a5ca60431
alperenkavukk <kavuk.67.76@gmail.com>
2022-07-24 18:18:34 +0300

    Initial Commit

---
+++ landmarkBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  landmarkBook
+//
+//  Created by Alperen Kavuk on 24.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
